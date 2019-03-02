9955cb313c9faa9b5169a67ede6fbaf8cefcf3ff
Amit Zimbalist <amitzim@gmail.com>
2019-03-02 11:38:46 +0200

    after CR fixes

--- linkedlist.c
+++ linkedlist.c
@@ -11,72 +11,82 @@
 
 // CR: a space before the {
 // CR: Most pepole would call this ListNode
-typedef struct Link{
-	struct Link *prev;
-	struct Link *next;
+typedef struct ListNode{
+	struct ListNode *prev;
+	struct ListNode *next;
 	int data;
-} Link;
+} ListNode;
 
 // CR: Normally this would be called ListHead
-typedef struct LinkedList {
-	struct Link *start;
+typedef struct ListHead {
+	struct ListNode *start;
+    struct ListNode *last;
 	// CR: It is nice that you used this exercise to also experiment with OOP in C :)
 	//Functions pointers
-	int(*add)(struct LinkedList *, int);
-	int(*remove)(struct LinkedList *, int);
-	void(*to_string)(struct LinkedList *);
+    //struct ListHead*(*list_init)();
+	int(*add)(struct ListHead *, int);
+	int(*remove)(struct ListHead *, int);
+	void(*to_string)(struct ListHead *);
 	void(*delete)();
-} LinkedList;
+} ListHead;
 
-int add(LinkedList *lst, int data) {
+int add(ListHead *lst, int data) {
 	// CR: Most pepole would expect you to use malloc here, but calloc is also great.
-	Link *new_link = (Link *)calloc(1, sizeof(Link));
+	ListNode *new_link = (ListNode *)calloc(1, sizeof(ListNode));
 	if (new_link == NULL) {
 		return FAIL;
 	}
 	new_link->data = data;
 	new_link->next = NULL;
-	Link *temp = lst->start;
-	if (temp == NULL) {
-		lst->start = new_link;
-		return SUCCESS;
-	}
+	ListNode *temp = lst->start;
+    if (temp == NULL) {
+        lst->start = new_link;
+        lst->last = new_link;
+        return SUCCESS;
+    }
 	// CR: It might be better to store a pointer to the last element of the list in the "LinkedList struct" to spare this loop. Normally adding an element to a linked list is considered O(1).
-	while (temp->next != NULL){
+	/*while (temp->next != NULL){
 		temp = temp->next;
-	}
-	temp->next = new_link;
-	new_link->prev = temp;
+	}*/
+    (lst->last)->next = new_link;
+    new_link->prev = lst->last;
+    lst->last = new_link;
 	return SUCCESS;
 }
 
-int remove_link(LinkedList *lst, int data) {
-	int counter = 0;
-	Link *temp = lst->start;
+int remove_link(ListHead *lst, int data) {
+	int status = FAIL;
+	ListNode *temp = lst->start;
 	while (temp != NULL) {
 		if (temp->data == data) {
 			// delete first link
-			if (temp->prev == NULL) {
-				temp = temp->next;
-				free(lst->start);
-				lst->start = temp;
-				continue;
+            if (temp->prev == NULL) {
+                temp = temp->next;
+                if (lst->start == lst->last) {
+                    lst->last = temp;
+                }
+                free(lst->start);
+                lst->start = temp;
+                continue;
 			// CR: I would put the } and the else in the same line. Like this:
 			// CR: } else {
 			// CR: But as long as you are consistent with your conventions it is great.
 			}
 			else {
-				Link *new_next = NULL;
+				ListNode *new_next = NULL;
 				if (temp->next != NULL) {
 					new_next = temp->next;
 				}
-				Link *prev = temp->prev;
+				ListNode *prev = temp->prev;
 				prev->next = new_next;
 				if (new_next != NULL) {
 					new_next->prev = prev;
 				}
+                else {
+                    lst->last = prev;
+                }
 				free(temp);
-				counter++;
+				status = SUCCESS;
 				temp = prev;
 			}
 		}
@@ -84,15 +94,16 @@ int remove_link(LinkedList *lst, int data) {
 	}
 	
 	// CR: If you don't use the counter for other purposes, it would be easier to read if you used a variable called status which is initialized to FAIL and update it to SUCCESS when you want. Counting here is just am implemetaiton detail.
-	if (counter > 0) {
+	/*if (status > 0) {
 		return SUCCESS;
 	}
-	return FAIL;
+	return FAIL;*/
+    return status;
 }
 
 
-void to_string(LinkedList *lst) {
-	Link *temp = lst->start;
+void to_string(ListHead *lst) {
+	ListNode *temp = lst->start;
 	while (temp != NULL) {
 		printf("%d ", temp->data);
 		temp = temp->next;
@@ -101,11 +112,15 @@ void to_string(LinkedList *lst) {
 }
 
 // CR: name this list_destroy ?
-void delete_list(LinkedList *self) {
+void list_destroy(ListHead *self) {
 	if (self == NULL) {
 		return;
 	}
-	Link *temp = self->start;
+    if (self->start == NULL) {
+        free(self);
+        return;
+    }
+	ListNode *temp = self->start;
 	while (temp->next != NULL) {
 		temp = temp->next;
 		free(temp->prev);
@@ -114,18 +129,28 @@ void delete_list(LinkedList *self) {
 	free(self);
 }
 
+struct ListHead *list_init() {
+    ListHead *lst = (ListHead *)calloc(1, sizeof(ListHead));
+    lst->start = NULL;
+    lst->add = &add;
+    lst->remove = &remove_link;
+    lst->to_string = &to_string;
+    lst->delete = &list_destroy;
+    return lst;
+}
+
 int main() {
 	// CR: I would have create a function to do that (list_init ?) aka a constructor for a list object.
-	LinkedList *lst = (LinkedList *)calloc(1, sizeof(LinkedList));
-	lst->start = NULL;
+	ListHead *lst = list_init();
+	/*lst->start = NULL;
 	lst->add = &add;
 	lst->remove = &remove_link;
 	lst->to_string = &to_string;
-	lst->delete = &delete_list;
+	lst->delete = &list_destroy;*/
 
 	for (int i = 1; i <= 20; i++) {
 		lst->add(lst, i);
-		lst->add(lst, 5);
+        lst->remove(lst, i);
 		/*add(lst, 5);
 		add(lst, i);*/
 	}
@@ -134,7 +159,6 @@ int main() {
 	printf("HI\n");
 	lst->to_string(lst);
 	lst->delete(lst);
-	//free(lst);
 	return 0;
 }
 
